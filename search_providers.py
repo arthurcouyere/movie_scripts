05d81870a84a6bc56f541b6a9379c3bbb0dac06f
Emmanuel Jammes <emmanuel.jammes@ratpsmartsystems.com>
2022-01-07 12:21:28 +0100

    added options : min_ratio, year_match, all

--- search_providers.py
+++ search_providers.py
@@ -19,7 +19,7 @@ from tqdm import tqdm
 ############################
 
 default_movie_extensions = [ "mkv", "mp4", "avi" ]
-min_fuzz_ratio = 70
+default_min_fuzz_ratio = 70
 default_content_type_list = ["show", "movie"]
 monetization_type_list = ["flatrate", "free"]
 default_provider_list = ["nfx", "prv", "dnp"]
@@ -84,59 +84,65 @@ def get_title_year_from_filename(filename: str) -> tuple:
 
     return (title, year)
 
-def search_content(query: str, content_type_list: list = default_content_type_list) -> list:
+def search_content(query: str, content_type_list: list = None, min_fuzz_ratio: int = None) -> list:
 
-        body = {
-            "query": query,
-            "content_types": content_type_list,
-            "monetization_types": monetization_type_list,
-            "providers": default_provider_list,
-            "enable_provider_filter": True,
-            "matching_offers_only": True,
-            "is_upcoming": False,
-            "page": 1,
-            "page_size": 5,
-        }
+    if content_type_list == None:
+        content_type_list = default_content_type_list
 
-        payload = {
-            "language": "en",
-            "body": json.dumps(body)
-        }
+    if min_fuzz_ratio == None :
+        min_fuzz_ratio = default_min_fuzz_ratio
 
-        # print(payload)
+    body = {
+        "query": query,
+        "content_types": content_type_list,
+        "monetization_types": monetization_type_list,
+        "providers": default_provider_list,
+        "enable_provider_filter": True,
+        "matching_offers_only": True,
+        "is_upcoming": False,
+        "page": 1,
+        "page_size": 5,
+    }
 
-        url = 'https://apis.justwatch.com/content/titles/fr_FR/popular'
-        r = requests.get(url, params=payload)
+    payload = {
+        "language": "en",
+        "body": json.dumps(body)
+    }
 
-        if r.status_code != 200:
-            raise Exception(f"ERROR {r.status_code}: {r.content}")
+    # print(payload)
+
+    url = 'https://apis.justwatch.com/content/titles/fr_FR/popular'
+    r = requests.get(url, params=payload)
+
+    if r.status_code != 200:
+        raise Exception(f"ERROR {r.status_code}: {r.content}")
+    
+    results = r.json()
+    search_result_list = results["items"]
+    content_list = []
+    
+    for search_result in search_result_list:
         
-        results = r.json()
-        search_result_list = results["items"]
-        content_list = []
-        
-        for search_result in search_result_list:
+        ratio = fuzz.ratio(search_result["title"], query.lower())            
+        if ratio >= min_fuzz_ratio:
+
+            provider_list = []
+            for offer in search_result["offers"]:
+                if offer["monetization_type"] in monetization_type_list \
+                    and offer["package_short_name"] in default_provider_list \
+                    and offer["package_short_name"] not in provider_list :
+                    provider_list.append(offer["package_short_name"])
             
-            ratio = fuzz.ratio(search_result["title"], query.lower())            
-            if ratio > min_fuzz_ratio:
+            content = Content(
+                title = search_result["title"],
+                ratio = ratio,
+                release_year = search_result["original_release_year"],
+                provider_list = provider_list
+            )
 
-                provider_list = []
-                for offer in search_result["offers"]:
-                    if offer["monetization_type"] in monetization_type_list \
-                        and offer["package_short_name"] in default_provider_list \
-                        and offer["package_short_name"] not in provider_list :
-                        provider_list.append(offer["package_short_name"])
-                
-                content = Content(
-                    title = search_result["title"],
-                    ratio = ratio,
-                    release_year = search_result["original_release_year"],
-                    provider_list = provider_list
-                )
+            content_list.append(content)
 
-                content_list.append(content)
-
-        return content_list
+    return content_list
             
 ############################
 # main
@@ -149,9 +155,12 @@ if __name__ == '__main__':
         # options
         parser = argparse.ArgumentParser(description='automatic providers search')
         parser.add_argument('extensions', nargs="*", help='movie file extensions')
-        parser.add_argument('-r', '--recursive', dest='recursive',action='store_true', help='recurse in sub folders')
-        parser.add_argument('-l', '--log',       dest='log',      action='store_true', help='log to file')
-        parser.add_argument('-v', '--verbose',   dest='verbose',  action='store_true', help='verbose mode')
+        parser.add_argument('-m', '--min-ratio',  metavar='min_ratio', type=int, help='minimun fuzz radio (default=%s)' % default_min_fuzz_ratio)
+        parser.add_argument('-y', '--year-match', dest='year_match', action='store_true', help='display only content matching year')
+        parser.add_argument('-a', '--all',        dest='all',        action='store_true', help='display all files, even if no content found')
+        parser.add_argument('-r', '--recursive',  dest='recursive',  action='store_true', help='recurse in sub folders')
+        parser.add_argument('-l', '--log',        dest='log',        action='store_true', help='log to file')
+        parser.add_argument('-v', '--verbose',    dest='verbose',    action='store_true', help='verbose mode')
 
         args = parser.parse_args()
         if len(args.extensions) == 0:
@@ -187,22 +196,24 @@ if __name__ == '__main__':
             (title, year) = get_title_year_from_filename(filepath_without_ext)
             logging.debug("title: [%s] year: %s" % (title, year))
 
-            content_list = search_content(query = title, content_type_list = ["movie"])
-            content_found = False
+            content_list = search_content(query = title, content_type_list = ["movie"], min_fuzz_ratio = args.min_ratio)
+
+            output = ""
 
             for content in content_list:
                 logging.debug("found: %s" % str(content))
                 if len(content.provider_list) > 0:
-                    if not content_found:
-                        tqdm.write("%s:" % (filepath))
-                        content_found = True
 
-                    if (year != None) and (year == content.release_year):
-                        color = "green"
-                    else:
-                        color = "yellow"
+                    year_matches = (year != None) and (year == content.release_year)
+                    color = "green" if year_matches else "yellow"
+                    
+                    if year_matches or not args.year_match:
+                        output += colored("\t" + str(content) + "\n", color)
 
-                    tqdm.write(colored("\t" + str(content), color))
+            if output or args.all:
+                tqdm.write(filepath)
+                tqdm.write(output, end="")
+                # content_found = True
                 
     # catch keyboard interrupt or broken pipe
     except (KeyboardInterrupt) as e:
